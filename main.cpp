82e95e881422352a87650a1f9e282cd57a6c84bb
Cobollatin <Cobollatin@hotmail.com>
2022-05-22 16:01:03 -0500

    UPPERCASE_VS_LOWERCASE

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+#define UPPERCASE_VS_LOWERCASE
+#define Some_other_snippet
+
 #include "solutions.hpp"
 
 
@@ -6,7 +9,9 @@ int main()
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
 
-    
+    solve();
+
+    system("pause");
 
     return 0;
 }
--- solutions.hpp
+++ solutions.hpp
@@ -1,1 +1,27 @@
 #include <iostream>
+#include <algorithm>
+#include <string>
+
+#ifdef UPPERCASE_VS_LOWERCASE
+void solve(){
+    std::string word;
+    std::cin >> word;
+    size_t counter{0};
+    for(auto& c : word){
+        if(std::isupper(c))
+        {
+            c = std::tolower(c);
+            counter++;
+        }
+    }
+    if(counter > word.length() / 2)
+        std::transform(word.begin(), word.end(), word.begin(), ::toupper);
+    std::cout << word << std::endl;
+}
+#endif
+
+#ifdef Some_other_snippet
+void solve(){
+
+}
+#endif
